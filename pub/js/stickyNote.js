d8cbc58ad8f949831089061baf7751da97f5c230
MY2333 <66840042+MY2333@users.noreply.github.com>
2021-12-18 18:37:02 -0500

    final

--- pub/js/stickyNote.js
+++ pub/js/stickyNote.js
@@ -1,24 +1,19 @@
-function stickyNote() {
+"use strict";
+
+(function(global, document, $) {
+    function stickyNote() {
     this.note;
+    this.divID = ''
     this.background = "black"
     this.color = "white"
     this.height = 300
     this.width = 200
+    this.type = ''
     this.fontSize = 15
     this.x = 0
     this.y = 0
     this.title = 'Sticky Note'
     this.italic = () => {
-        // const text = document.getElementById("textArea").innerHTML
-        // const selected = window.getSelection();
-        // const start = selected.anchorOffset
-        // const end = selected.focusOffset
-        // const sub1 = text.substring(0, start)
-        // const sub2 = '<i>' + selected.toString() + '</i>'
-        // const sub3 = text.substring(end)
-        // document.getElementById("textArea").innerHTML = sub1 + sub2 + sub3
-        // console.log(start)
-        // console.log(end)
         document.execCommand("italic")
     }
     this.underline=() => {
@@ -28,12 +23,12 @@ function stickyNote() {
         document.execCommand("bold")
     }
     this.zoom = (event) => {
-        cur = event.target.parentElement
+        var cur = event.target.parentElement
         cur.style.display = 'none'
         cur.parentElement.lastChild.style.display = 'block'
     }
     this.show = (event) => {
-        cur = event.target
+        var cur = event.target
         cur.style.display = 'none'
         cur.parentElement.firstChild.style.display = 'block'
     }
@@ -64,45 +59,65 @@ function changeFontColorDom(n, color){
 }
 
 stickyNote.prototype = {
-	create: function() {
+	create: function(divID, width, height, type) {
+        this.divID = divID
+        this.width = width
+        this.height = height
+        this.type = type
+
         const n = document.createElement('div')
+        n.id = this.divID
         n.style.backgroundColor = this.background
         n.style.position = "fixed"
         n.style.top = this.x
-        n.style.left=this.y
+        n.style.left= this.y
         n.style.color=this.color
+
 		const note = document.createElement('div')
         note.style.width = this.width
         note.style.height = this.height
         note.style.padding = '10px'
         const b3 = document.createElement('button')
         b3.style = "width:30px; height: 20px; float:right"
+
         b3.innerHTML = "-"
         b3.onclick = this.zoom
         note.append(b3)
+
         const title = document.createElement('p')
         title.innerHTML = this.title
+        title.style.cursor = 'move'        
         note.append(title)
+
 		const b1 = document.createElement('button')
         b1.innerHTML = "<u>U</u>"
         b1.style = "width:30px; height: 20px; margin:5px"
         b1.onclick = this.underline
         note.append(b1)
+
         const b2 = document.createElement('button')
         b2.onclick = this.italic
         b2.style = "width:30px; height: 20px; margin: 5px"
         b2.innerHTML = "<i>I</i>"
         note.append(b2)
+
         const b4 = document.createElement('button')
         b4.onclick = this.bold
         b4.style = "width:30px; height: 20px; margin: 5px"
         b4.innerHTML = "<b>B</b>"
         note.append(b4)
+
         const texta = document.createElement('div')
         texta.setAttribute("contenteditable", true);
-        texta.style = 'width: 190px; height:180px; border-style: solid; overflow-y: scroll;'
+        texta.style = 'border-style: solid; overflow-y: scroll;'
+        if(this.type == 'bullets'){
+            texta.innerHTML = '<ul><li></li></ul>'
+        }else if(this.type == 'numbers'){
+            texta.innerHTML = '<ol><li></li></ol>'
+        }
         note.append(texta)
         n.append(note)
+
         const show = document.createElement('button')
         show.onclick = this.show
         show.style = "width:30px; height: 20px; margin: 5px; display: none"
@@ -111,6 +126,32 @@ stickyNote.prototype = {
 		this.note = n
 		const body = $('body')
         body.append(n)
+        
+        this.changeSize(this.height, this.width)
+        
+        var isDown = false
+        var offset = [0, 0]
+        title.addEventListener('mousedown', function(event) {
+            isDown = true;
+            offset = [
+                title.parentElement.parentElement.offsetLeft - event.clientX,
+                title.parentElement.parentElement.offsetTop - event.clientY
+            ];
+            
+        }, true);
+        
+        title.addEventListener('mouseup', function() {
+            isDown = false;
+        }, true);
+        
+        title.addEventListener('mousemove', function(event) {
+            event.preventDefault();
+            if (isDown) {
+                title.parentElement.parentElement.style.left = (event.clientX + offset[0]) + 'px';
+                title.parentElement.parentElement.style.top  = (event.clientY + offset[1]) + 'px';
+            }
+        }, true);
+
 	},
 
 	changeColor: function(color) {
@@ -141,3 +182,5 @@ stickyNote.prototype = {
         changeFontColorDom(this, color)
     }
 }
+global.stickyNote = global.stickyNote || stickyNote
+})(window, window.document, $);
